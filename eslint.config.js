9e55de60ff689ae037efdf62862b3d3d13b30964
Madan Sapkota <contact@madansapkota.com>
2024-02-08 17:04:17 +0545

    Add eslint config file

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['dist', 'public', 'node_modules'],
+    files: ['makers/*.ts', 'src/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      'prefer-const': 'error',
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
+];
