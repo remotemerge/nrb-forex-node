21cd74700aba8842067fb19b2de2810a247cecf4
Madan Sapkota <contact@madansapkota.com>
2024-02-09 13:09:36 +0545

    Add method to fetch rate of specific day

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,58 @@
 import { apiUrl } from './helper/config';
 import { apiError } from './helper/message';
-import { StandardRate } from './types';
+import { DateRate, StandardRate } from './types';
 
-const liveRate = async (iso3 = ''): Promise<StandardRate | StandardRate[]> => {
+const liveRate = async (
+  inputIso3 = '',
+): Promise<StandardRate | StandardRate[]> => {
   const res = await fetch(`${apiUrl}/app-rate`);
   if (!res.ok) {
     throw new Error(apiError);
   }
+
   // Load the data from the response
   const data = <StandardRate[]>await res.json();
 
   // Filter the data based on the iso3 parameter
-  if (iso3) {
+  if (inputIso3) {
     const [match] = data.filter(
-      (rate) => rate.iso3.toUpperCase() === iso3.toUpperCase(),
+      (rate) => rate.iso3.toUpperCase() === inputIso3.toUpperCase(),
     );
     return match;
   }
   return data;
 };
 
-export { liveRate };
+const dayRate = async (
+  inputDate: string,
+  inputIso3 = '',
+): Promise<StandardRate | StandardRate[]> => {
+  const res = await fetch(`${apiUrl}/rate?date=${inputDate}`);
+  if (!res.ok) {
+    throw new Error(apiError);
+  }
+
+  // Load the data from the response
+  const dayRes = <DateRate>await res.json();
+
+  // Extract date data
+  const { date, published_on, modified_on, rates } = dayRes.data.payload;
+
+  // Merge the date with rates
+  const data = rates.map(({ buy, sell, currency }) => ({
+    ...currency,
+    ...{ buy: +buy, sell: +sell },
+    ...{ date, published_on, modified_on },
+  }));
+
+  // Filter the data based on the iso3 parameter
+  if (inputIso3) {
+    const [match] = data.filter(
+      (rate) => rate.iso3.toUpperCase() === inputIso3.toUpperCase(),
+    );
+    return match;
+  }
+  return data;
+};
+
+export { liveRate, dayRate };
