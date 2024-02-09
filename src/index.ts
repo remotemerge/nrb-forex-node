cd864ddc744ec999397f879cda4a136cb80aed03
Madan Sapkota <contact@madansapkota.com>
2024-02-09 12:42:54 +0545

    Fetch live forex data

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { apiUrl } from './helper/config';
+import { apiError } from './helper/message';
+import { StandardRate } from './types';
+
+const live = async (iso3 = ''): Promise<StandardRate | StandardRate[]> => {
+  const res = await fetch(`${apiUrl}/app-rate`);
+  if (!res.ok) {
+    throw new Error(apiError);
+  }
+  // Load the data from the response
+  const data = <StandardRate[]>await res.json();
+
+  // Filter the data based on the iso3 parameter
+  if (iso3) {
+    const [match] = data.filter(
+      (rate) => rate.iso3.toUpperCase() === iso3.toUpperCase(),
+    );
+    return match;
+  }
+  return data;
+};
+
+export { live };
