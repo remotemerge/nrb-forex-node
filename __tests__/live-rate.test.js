00b85c647ce876b48c7664b855a581dc52821e5f
Madan Sapkota <contact@madansapkota.com>
2024-02-10 08:15:14 +0545

    Update test cases

--- __tests__/live-rate.test.js
+++ __tests__/live-rate.test.js
@@ -2,23 +2,18 @@ import { test } from 'vitest';
 import assert from 'assert';
 import { liveRate } from '../dist/nrb-forex.js';
 
-test('it contains multiple rates', () => {
-  liveRate().then((data) => {
-    assert.equal(data.rates.length > 0, true);
-  });
+test('it contains multiple rates', async () => {
+  const res = await liveRate();
+  assert.equal(res.rates.length > 0, true);
 });
 
-test('it contains a rate for USD', () => {
-  liveRate().then((data) => {
-    assert.equal(
-      data.rates.find((rate) => rate.currency === 'USD'),
-      true,
-    );
-  });
+test('it contains a rate for USD', async () => {
+  const res = await liveRate();
+  const usdRate = res.rates.find((rate) => rate.currency.iso3 === 'USD');
+  assert.equal(usdRate.currency.iso3, 'USD');
 });
 
-test('it contains a rate for EUR', () => {
-  liveRate('EUR').then((data) => {
-    assert.equal(data.rate.currency === 'EUR', true);
-  });
+test('it contains a rate for EUR', async () => {
+  const res = await liveRate('EUR');
+  assert.equal(res.rate.currency.iso3, 'EUR');
 });
