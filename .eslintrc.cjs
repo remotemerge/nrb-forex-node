3a9850b50673602aa21c4ba2ce6b494644c0f4ce
Madan Sapkota <contact@madansapkota.com>
2024-07-12 10:10:51 +0545

    Update ESLint configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2023: true,
     node: true,
+    jest: true,
+    es2024: true,
   },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
+    // Prettier rules
     'prettier/prettier': 'error',
-    indent: ['error', 2, { SwitchCase: 1 }],
+    // Custom rules
+    indent: ['error', 2, { SwitchCase: 1, ignoredNodes: ['PropertyDefinition'] }],
     'linebreak-style': ['error', 'unix'],
     'prefer-const': 'error',
     quotes: ['error', 'single'],
