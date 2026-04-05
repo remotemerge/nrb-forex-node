9dd91c4bc7c3724929a87748ec10d2f5108f1785
Madan Sapkota <contact@madansapkota.com>
2026-04-05 16:53:26 +0545

    Simplify environment variable loading

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [dts({ include: 'src/*.ts' })],
     build: {
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '^/api/forex/v1/(.+)': {
-          target: env.VITE_NRB_API_URL,
+          target: env.NRB_API_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace('/api/forex/v1', ''),
         },
