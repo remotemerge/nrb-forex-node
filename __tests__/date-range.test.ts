7975d95739e51cef4bef8e3f4085b857d578b13f
Madan Sapkota <contact@madansapkota.com>
2026-04-05 16:11:48 +0545

    Update tests to use Bun and replace Vitest

--- __tests__/date-range.test.ts
+++ __tests__/date-range.test.ts
@@ -1,16 +1,16 @@
-import { test, assert } from 'vitest';
-import { DateRangeRate } from '../dist/types';
-import { dateRangeRate } from '../dist/nrb-forex.js';
+import { test, expect } from 'bun:test';
+import type { DateRangeRate } from '../src/types';
+import { dateRangeRate } from '../src';
 
 test('it contains multiple days records', async () => {
   const res = <DateRangeRate>await dateRangeRate({
     from: '2024-02-02',
     to: '2024-02-10',
     page: 1,
-    perPage: 5,
+    perPage: 5
   });
 
-  assert.equal(res.payload.length > 0, true);
+  expect(res.payload.length > 0).toBe(true);
 });
 
 test('it contains a rate for KRW', async () => {
@@ -18,13 +18,13 @@ test('it contains a rate for KRW', async () => {
     from: '2024-02-02',
     to: '2024-02-10',
     page: 1,
-    perPage: 5,
+    perPage: 5
   });
 
   const [firstData] = res.payload;
   const koreanRate = firstData.rates.find(
-    (rate) => rate.currency.iso3 === 'KRW',
+    (rate) => rate.currency.iso3 === 'KRW'
   );
 
-  assert.equal(koreanRate!.currency.iso3, 'KRW');
+  expect(koreanRate!.currency.iso3).toBe('KRW');
 });
--- __tests__/date-rate.test.ts
+++ __tests__/date-rate.test.ts
@@ -1,19 +1,19 @@
-import { test, assert } from 'vitest';
-import { StandardRate } from '../dist/types';
-import { dateRate } from '../dist/nrb-forex.js';
+import { test, expect } from 'bun:test';
+import type { StandardRate } from '../src/types';
+import { dateRate } from '../src';
 
 test('it contains multiple rates', async () => {
   const res = <StandardRate>await dateRate({ date: '2024-02-10' });
-  assert.equal(res.rates!.length > 0, true);
+  expect(res.rates!.length > 0).toBe(true);
 });
 
 test('it contains a rate for CHF', async () => {
   const res = <StandardRate>await dateRate({ date: '2024-02-10' });
   const swissRate = res.rates!.find((rate) => rate.currency.iso3 === 'CHF');
-  assert.equal(swissRate!.currency.iso3, 'CHF');
+  expect(swissRate!.currency.iso3).toBe('CHF');
 });
 
 test('it contains a rate for MYR', async () => {
   const res = <StandardRate>await dateRate({ date: '2024-02-10', iso3: 'MYR' });
-  assert.equal(res.rate!.currency.iso3, 'MYR');
+  expect(res.rate!.currency.iso3).toBe('MYR');
 });
